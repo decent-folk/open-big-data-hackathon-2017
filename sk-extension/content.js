e2e947e0f65273bb45a4159a4df79d813cd5d4a6
Amin Benarieb <amin.benarieb@gmail.com>
2017-03-05 00:49:01 +0300

    Added different styles for skills depending on resume skills.

--- sk-extension/content.js
+++ sk-extension/content.js
@@ -1,5 +1,7 @@
 function sk_span_skill(element){
 	return "<span class=\"sk_skill\">"+element+"</span>"
+function sk_span_skill(element, skill_style){
+	return "<span class=\""+skill_style+" sk_skill\">"+element+"</span>"
 }
 
 function sk_request(query) {
@@ -14,15 +16,26 @@ var search_input = "input.bloko-input"
 
 window.onload = function() {
 
-	query = $( search_input ).first().val()
-
+	let query = $( search_input ).first().val();
 	$.getJSON( sk_request(query), function( data ) {
 
 		// var skills = data["requirements"]
 		var skills = ["UIKit", "Cocoa Touch", "Objective-C"];
 
-		skills.forEach(function(skill) {
-			$( ".search-result" ).prepend( sk_span_skill(skill) );
+
+		let resume_id = "fa318ae9ff0314d9a60039ed1f6e623356535a";
+		$.getJSON( sk_resume_request(resume_id), function( data ) {
+
+			var resume_skills = data["skill_set"]
+			skills.forEach(function(skill) {
+
+				var skill_style = resume_skills.contains(skill) ? "sk_skill_match" : "sk_skill_missed"
+
+				$( ".search-result" ).prepend( sk_span_skill(skill, skill_style) );
+			});
+
 		});
+
 	});
+
 }
