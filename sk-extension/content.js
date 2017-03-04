b21568ff226646dfcef03d448d0835be8e11376f
Amin Benarieb <amin.benarieb@gmail.com>
2017-03-05 01:12:25 +0300

    Refactoring.

--- sk-extension/content.js
+++ sk-extension/content.js
@@ -1,49 +1,33 @@
-Array.prototype.contains = function(obj) {
-    var i = this.length;
-    while (i--) {
-        if (this[i] === obj) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function sk_span_skill(element, skill_style){
-	return "<span class=\""+skill_style+" sk_skill\">"+element+"</span>"
-}
-
-function sk_request(query) {
-	return "https://api.hh.ru/vacancies/"
-}
-
-function sk_resume_request (resume_id) {
-	return "https://api.hh.ru/resumes/" + resume_id
-}
-
-var search_input = "input.bloko-input"
-
 window.onload = function() {
 
-	let query = $( search_input ).first().val();
-	$.getJSON( sk_request(query), function( data ) {
+	let query = SK.search_input().val();
+	console.log( SK.request(query) )
+	$.ajax({
+            url:        SK.request(query),
+            dataType:   "json",
+            success:    function(data){
 
-		// var skills = data["requirements"]
-		var skills = ["UIKit", "Cocoa Touch", "Objective-C", "ООП", "Crashlytics"];
+            	console.log( query );
 
+                var skills = data["requirements"]
+				// var skills = ["UIKit", "Cocoa Touch", "Objective-C", "ООП", "Crashlytics"];
 
-		let resume_id = "fa318ae9ff0314d9a60039ed1f6e623356535a";
-		$.getJSON( sk_resume_request(resume_id), function( data ) {
+				let resume_id = "fa318ae9ff0314d9a60039ed1f6e623356535a";
+				$.getJSON( SK.resume_request(resume_id), function( data ) {
 
-			var resume_skills = data["skill_set"]
-			skills.forEach(function(skill) {
+					var resume_skills = data["skill_set"]
+					skills.forEach(function(skill) {
 
-				var skill_style = resume_skills.contains(skill) ? "sk_skill_match" : "sk_skill_missed"
+						var skill_style = resume_skills.contains(skill) ? "sk_skill_match" : "sk_skill_missed"
 
-				$( ".search-result" ).prepend( sk_span_skill(skill, skill_style) );
-			});
-
-		});
-
-	});
-
+						SK.search_results().prepend( SK.span_skill(skill, skill_style) );
+					});
+				});
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+            	console.log(jqXHR.status);
+            	console.log(textStatus);
+            }/*,
+            headers: {'Host': kServerURL}*/
+        });
 }
---
+++ sk-extension/sk.js
@@ -0,0 +1,30 @@
+const kSearchInput = "input.bloko-input"
+const kSearchResults = ".search-result"
+const kServerURL = "http://185.158.153.129:5000"
+
+class SK {}
+
+SK.span_skill = function (element, skill_style){
+	return "<a href=\"#\" display=\"block\"><span class=\""+skill_style+" sk_skill\">"+element+"</span></a>"
+}
+
+SK.request = function(query) {
+	// return "https://api.hh.ru/vacancies/"
+	return kServerURL+"/getRequirements?name="+query
+}
+
+SK.resume_request = function (resume_id) {
+	return "https://api.hh.ru/resumes/" + resume_id
+}
+
+SK.loading_view = function () {
+	return "<span id=\"loading_view\">Загружаем большие данные...</span>"
+}
+
+SK.search_input = function() {
+	return $( kSearchInput ).first()
+}
+
+SK.search_results = function() {
+	return $( kSearchResults ).first()
+}
