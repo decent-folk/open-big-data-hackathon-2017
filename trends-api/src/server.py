fcc20101a5b255a44b34f349eb995419f028d1dc
okondrashin <kondrashin96@gmail.com>
2017-03-05 11:39:12 +0300

    Add /getTrends method realization

--- trends-api/src/server.py
+++ trends-api/src/server.py
@@ -5,6 +5,11 @@ from flask import make_response
 
 import json
 
+import pytrends
+from pytrends.request import TrendReq
+
+import pandas
+
 app = Flask(__name__)
 
 @app.errorhandler(400)
@@ -21,7 +26,35 @@ def get_requirements():
     if (skill is None):
         abort(400)
 
-    resp = make_response()
+    pytrends = TrendReq(
+        "hackathonspb@gmail.com",
+        "qwertyqwerty",
+        hl='en-US',
+        tz=360,
+        custom_useragent=None
+    )
+
+    pytrends.build_payload(
+        [skill],
+        cat=0,
+        timeframe='today 5-y',
+        geo='',
+        gprop=''
+    )
+
+    interest_over_time_df = pytrends.interest_over_time()
+
+    resp = make_response(interest_over_time_df.to_json(
+        path_or_buf=None,
+        orient=None,
+        date_format='epoch',
+        double_precision=10,
+        force_ascii=True,
+        date_unit='ms',
+        default_handler=None,
+        lines=False)
+    )
+
     resp.headers['Access-Control-Allow-Origin'] = "*"
 
     return resp
